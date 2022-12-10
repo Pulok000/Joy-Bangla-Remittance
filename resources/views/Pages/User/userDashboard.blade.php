3acf6bbe05360c34c24fe76a6d28ea8924869c5a
Pulok Ahmed <pulokahmed07@gmail.com>
2022-12-10 13:29:14 +0600

    8: Adding mix file

--- resources/views/Pages/User/userDashboard.blade.php
+++ resources/views/Pages/User/userDashboard.blade.php
@@ -6,7 +6,17 @@
 <div class="custom-login">
     <div class="center">
 
+        <!-- start  -->
 
+        <div id="example">
+
+        </div>
+
+
+        <!-- React JS -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- end  -->
 
 
     </div>
